22ebef72ba7f4ed67c7f79bc20dfed9934eb5ae1
Michał Kiełkowski <mkielkowski01@gmail.com>
2023-08-30 00:53:06 +0200

    improving constructors and cleaning up code

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/michalkielkowski/Desktop/infa-all/scripts/c++-script-dairy
+# Install script for directory: /Users/michalkielkowski/Desktop/infa-all/scripts/c++-web-scripting
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/michalkielkowski/Desktop/infa-all/scripts/c++-script-dairy/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/michalkielkowski/Desktop/infa-all/scripts/c++-web-scripting/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ examples/penzu_dairy_autonomation/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+#include "program.h"
+#include "mouse.h"
+#include "keyboard.h"
+#include <chrono>
+#include <thread>
+#include <ctime>
+#include <iomanip>
+
+int main() {
+
+    system("open https://penzu.com/app/login?after_login=%2Fjournals");
+    usleep(5000000); // Sleep for 5 second (adjust as needed)
+    system("screencapture input1.png");
+
+    program penzu(0, 0);
+
+    penzu.temple_matching("/Users/michalkielkowski/Desktop/infa-all/scripts/c++-web-scripting/examples/penzu_dairy_autonomation/find1.png", "/Users/michalkielkowski/Desktop/infa-all/scripts/c++-web-scripting/examples/penzu_dairy_autonomation/cmake-build-debug/input1.png" );
+
+    penzu.get_x_cord();
+    penzu.get_y_cord();
+
+    penzu.move_mouse();
+    usleep(1000000); // Sleep for 1 second (adjust as needed)
+    penzu.left_mouse_click();
+    usleep(5000000); // Sleep for 1 second (adjust as needed)
+    time_t currentTime = time(nullptr);
+    tm *localTime = localtime(&currentTime);
+    int day = localTime->tm_mday;
+    int month = localTime->tm_mon + 1;
+    string formattedDate = to_string(day) + "." + (month < 10 ? "0" : "") + to_string(month);
+    penzu.write_input(formattedDate, 100);
+    usleep(1000000); // Sleep for 1 second (adjust as needed)
+    penzu.move_mouse_to_specific_point(penzu.get_x_cord(), penzu.get_y_cord() - 200);
+    usleep(1000000); // Sleep for 1 second (adjust as needed)
+    penzu.left_mouse_click();
+    usleep(1000000); // Sleep for 1 second (adjust as needed)
+    penzu.write_input("How was your day:\nWhat did you learn today:\nHow was the day in a scale 1-10:", 100);
+    remove("/Users/michalkielkowski/Desktop/infa-all/scripts/c++-web-scripting/examples/penzu_dairy_autonomation/cmake-build-debug/input1.png");
+    return 0;
+}
